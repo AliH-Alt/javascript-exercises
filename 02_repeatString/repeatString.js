162b9c83021374b1e68c6bf24033515c75f6082e
AliH-Alt <[raffhpriv@gmail.com]>
2024-08-06 22:28:03 -0700

    first three exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(input = '', number = 0) {
+    if (number < 0){
+        return 'ERROR'
+    }
+    
+    repeatedStr = ''
+    for(number; number > 0; number--){
+        repeatedStr += input;
+    }
 
+    return repeatedStr
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(input = "") {
+    let revStr = ""
+    let splitArr = input.split(" ")
+    for(let wordIndex in splitArr){
+        for(let i = splitArr.at(wordIndex).length - 1; i >= 0; i--){
+            revStr += splitArr.at(wordIndex).at(i)
+        }
+    }
+    console.log(revStr)
 };
+
+reverseString("hello world")
 
 // Do not edit below this line
 module.exports = reverseString;
